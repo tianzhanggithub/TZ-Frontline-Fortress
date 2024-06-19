34c5c3b61465c803049da2242c1287d81aa8d391
songjunbo <3096798109@qq.com>
2024-06-19 21:11:20 +0800

    init

---
+++ tzff-start/src/main/java/per/tz/frontlinefortress/TZFFStartApplication.java
@@ -0,0 +1,19 @@
+package per.tz.frontlinefortress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * @author 天章
+ * @Date 2024/6/19 13:24
+ * Description: ...
+ */
+@SpringBootApplication
+public class TZFFStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TZFFStartApplication.class, args);
+    }
+}
+
